7ca4fce5c2b793e21f8c8f9f635b1cfecf4443fd
Flávio Costa <flaviojcostafilho@gmail.com>
2024-06-18 23:09:43 -0300

    feat: Add create loan command

---
+++ LibraryManager.Application/Commands/CreateLoan/CreateLoanCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace LibraryManager.Application.Commands.CreateLoan
+{
+    public class CreateLoanCommand(Guid userId, Guid bookId) : IRequest<Guid>
+    {
+        public Guid UserId { get; private set; } = userId;
+        public Guid BookId { get; private set; } = bookId;
+    }
+}
