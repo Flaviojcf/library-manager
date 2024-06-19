ecf86a668982076168476416cec4f3bce64a5601
Flávio Costa <flaviojcostafilho@gmail.com>
2024-06-19 17:51:18 -0300

    feat: Create new migration

---
+++ LiBraryManager.Infrastructure/Migrations/20240619205101_AddNewPropertiesToBookDomain.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiBraryManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewPropertiesToBookDomain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AvailableQuantity",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Books",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalQuantity",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvailableQuantity",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "TotalQuantity",
+                table: "Books");
+        }
+    }
+}
